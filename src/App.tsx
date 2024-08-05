16f2c485c28229195523c6658abdbb8329e974b8
LucasGSeabra <lucasgseabra@gmail.com>
2024-08-05 09:21:10 -0300

    Update adds material-ui cssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
+import { CssBaseline } from "@mui/material";
+
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <Header />
     </div>
   );
